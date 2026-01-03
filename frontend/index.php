db805c1e950fb3fa12223e312dfe211d1fad7e72
satyam-javascript <javascriptes22@gmail.com>
2026-01-03 19:24:41 +0530

    making header and hero section and design both section

---
+++ frontend/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CarTreands.com</title>
+<link rel="stylesheet" href="./assets/style.css"/>
+</head>
+<body>
+    <header class="header">
+        <div class="container">
+            <nav class="navbar">
+                <div class="logo">
+                   <h1>CarTrands</h1>
+                   <span>Reference: carsdekho.com</span>
+                </div>
+                <ul class="nav-links">
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#Menu">Menu</a></li>
+                    <li><a href="#About">About</a></li>
+                    <li><a href="#Contact-Us">Contact-Us</a></li>
+
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <main>
+        <section id="home" class="hero">
+            <div class="hero-content">
+                <h2>Welcome To Site</h2>
+                <p>Experienc the racing car with me and my friends jame luke</p>
+                <div class="hero-button">
+                    <a href="#menu" class="btn btn-primary">Join With US</a>
+                    <a href="#contact" class="btn btn-secondery">Explore</a>
+                </div>
+            </div>
+        </section>
+    </main>
+</body>
+</html>
